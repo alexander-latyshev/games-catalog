90b185f47ac4c5888963c74ae125b26c4501a23b
Alexander Latyshev <aladdelinxr@gmail.com>
2022-12-26 23:17:22 +0300

    Add base layout

--- components/main.tsx
+++ components/main.tsx
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { fetchGames, fetchPlatforms } from "../redux/reducers/dataSlice";
 import theme from "../styles/theme";
+import Aside from "./aside";
+import ProductCard from "./product/productCard";
+import ProductList from "./product/productList";
 
 const MainContainer = styled.main`
   display: flex;
+  padding: 0 40px;
+  min-height: 100vh;
+  max-height: 100%;
+  justify-content: space-between;
+  background-color: ${({ theme }) => theme.background};
+  color: ${({ theme }) => theme.white};
 `;
 
 const Title = styled.h1`
-  color: ${({ theme }) => theme.darkGrey};
-  font-size: 30px;
+  font-size: 72px;
+`;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Subtitle = styled.p`
+  font-size: 16px;
+  color: ${({ theme }) => theme.white};
 `;
 
 const Main = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPlatforms());
+    dispatch(fetchGames());
+  }, []);
+
   return (
     <MainContainer>
-      <Title>Main</Title>
+      <Aside />
+      <Section>
+        <Title>New and trending</Title>
+        <Subtitle>Based on player counts and release date</Subtitle>
+        <ProductList />
+      </Section>
     </MainContainer>
   );
 };
