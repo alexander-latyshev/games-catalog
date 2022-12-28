6a8c602f2ab5d7bdd063cf2914ec5ba8a56855f9
Alexander Latyshev <aladdelinxr@gmail.com>
2022-12-28 18:21:49 +0300

    Add masonry product list

--- components/product/productCard.tsx
+++ components/product/productCard.tsx
@@ -1,63 +1,100 @@
 import React from "react";
 import styled from "styled-components";
-import { IPlatform } from "../../models/platforms";
-import PlatformButton from "./platformButton";
+import { Platform } from "../../models/games";
 
 type Props = {
   id: number;
   name: string;
   image: string;
-  platforms: IPlatform[];
+  platforms: Platform[];
+  released: string;
+  rating?: number | null;
+  metacritic?: number;
 };
 
 const Card = styled.div`
   color: ${({ theme }) => theme.secondary};
-  padding: 16px 16px 24px;
   flex-direction: column;
+  max-width: 270px;
   margin-top: 24px;
   display: flex;
   width: 100%;
   cursor: pointer;
   background-color: #202020;
+  border-radius: 12px;
+  transition: box-shadow 0.2s ease-in-out;
+  -webkit-box-shadow: 0 10px 20px 0 rgb(0 0 0 / 7%);
+  box-shadow: 0 10px 20px 0 rgb(0 0 0 / 7%);
+  :hover {
+    box-shadow: 0 4px 20px hsl(210deg 5% 37% / 19%);
+  }
 `;
 
 const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
+  padding: 16px 16px 24px;
 `;
 
 const Title = styled.h3`
   width: 100%;
   color: ${({ theme }) => theme.white};
-  text-align: center;
   margin-bottom: 8px;
   overflow: hidden;
   text-overflow: ellipsis;
-  padding-bottom: 2px;
   margin-top: 10px;
+  font-size: 24px;
+  line-height: 28px;
+  font-weight: 700;
 `;
 
 const Icon = styled.img`
   width: 100%;
   height: 100%;
+  min-height: 150px;
   object-fit: cover;
+  margin-bottom: 10px;
   border-top-left-radius: 12px;
   border-top-right-radius: 12px;
-  margin-bottom: 10px;
+`;
+
+const Text = styled.span`
+  color: ${({ theme }) => theme.white};
+`;
+
+const Info = styled(Wrapper)`
+  flex-direction: column;
+  padding: 0;
+`;
+
+const Rating = styled.div`
+  color: ${({ theme }) => theme.lightGreen};
+  border: 1px solid ${({ theme }) => theme.darkGreen};
+  margin-left: auto;
+  padding: 2px 0;
+  min-width: 32px;
+  border-radius: 4px;
+  margin-top: 20px;
+  font-weight: 700;
+  text-align: center;
+  border: 1px solid;
 `;
 
 const ProductCard = (props: Props) => {
-  console.log(props.platforms);
-
   return (
     <Card>
       <Icon src={props.image} />
       <Wrapper style={{ display: "flex" }}>
-        {props.platforms?.map((item: any) => {
-          return <PlatformButton name={item.platform.name} />;
-        })}
+        {/* {props.platforms?.map((item: IAvailablePlatforms, index: number) => {
+          return <PlatformButton key={index} name={item.platform.name} />;
+        })} */}
+        <Title>{props.name}</Title>
+
+        <Info>
+          <Text>{`Release date: ${props.released}`}</Text>
+        </Info>
+        {props.metacritic && <Rating>{props.metacritic}</Rating>}
       </Wrapper>
-      <Title>{props.name}</Title>
     </Card>
   );
 };
--- components/product/productList.tsx
+++ components/product/productList.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Masonry from "react-masonry-css";
 import styled from "styled-components";
-import { useAppSelector } from "../../redux/hooks";
+import { IGame } from "../../models/games";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { fetchGames, fetchPlatforms } from "../../redux/reducers/dataSlice";
 import ProductCard from "./productCard";
 
 const LayoutGrid = styled.div`
   .my-masonry-grid {
     display: flex;
+    width: 858px;
   }
   .my-masonry-grid_column:nth-child(2) {
     margin: 0 24px;
@@ -14,7 +17,38 @@ const LayoutGrid = styled.div`
 `;
 
 const ProductList = () => {
-  const games = useAppSelector((state) => state.data.games);
+  const games: IGame[] = useAppSelector((state) => state.data.games);
+  const dispatch = useAppDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [items, setItems] = useState([] as IGame[]);
+  const [fetching, setFetching] = useState(true);
+
+  useEffect(() => {
+    if (items?.length === 0) setItems(games);
+    document.addEventListener("scroll", scrollHandler);
+    return () => {
+      document.removeEventListener("scroll", scrollHandler);
+    };
+  }, [games]);
+
+  const scrollHandler = () => {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    console.log(scrollHeight - (scrollTop + window.innerHeight));
+    if (scrollHeight - (scrollTop + window.innerHeight) < 700) {
+      setFetching(true);
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (fetching) {
+      dispatch(fetchGames(currentPage));
+      setItems([...items, ...games]);
+      setFetching(false);
+    }
+  }, [fetching]);
+
   return (
     <LayoutGrid>
       <Masonry
@@ -22,14 +56,17 @@ const ProductList = () => {
         className="my-masonry-grid"
         columnClassName="my-masonry-grid_column"
       >
-        {games?.map((game: any, index: number) => {
+        {items?.map((game: IGame, index: number) => {
           return (
             <ProductCard
               key={index}
               name={game.name}
               id={game.id}
-              image={game?.background_image}
-              platforms={game?.platforms}
+              image={game.background_image}
+              platforms={game.platforms}
+              released={game.released}
+              metacritic={game.metacritic}
+              rating={game.rating}
             />
           );
         })}
