0f6ec6fdcdcdb0e94119f6edf699cff09938172d
Alexander Latyshev <aladdelinxr@gmail.com>
2022-12-30 00:50:43 +0300

    Update project themes

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,15 +1,19 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  primary: "#040714",
-  background: "#151515",
+  primary: "#151515",
   secondary: "#202020",
   lightGreen: "#6dc849",
   darkGreen: "rgba(109, 200, 73, 0.4)",
+  lightOrange: "#fdca52",
+  darkOrange: "rgba(253, 202, 82, 0.4)",
+  lightRed: "#c80000",
+  darkRed: "rgb(200, 0, 0, 0.4);",
   white: "#fff",
   silver: "silver",
   lightGrey: "hsla(0,0%,100%,.16)",
   darkGrey: "#31343e",
+  hover: "hsl(210deg 5% 37% / 19%)",
 };
 
 export default theme;
